c2daf612b662c777fa79d1ab735ce7853eef2c3e
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-03-31 16:55:24 -0700

    update date in copyright notices

--- app/src/main/java/com/ultramegatech/ey/provider/DatabaseOpenHelper.java
+++ app/src/main/java/com/ultramegatech/ey/provider/DatabaseOpenHelper.java
@@ -1,6 +1,6 @@
 /*
  * The MIT License (MIT)
- * Copyright © 2015 Steve Guidetti
+ * Copyright © 2016 Steve Guidetti
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the “Software”), to deal
