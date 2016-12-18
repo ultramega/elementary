c97b07341a3c46fa1eeffea52aac718d0bade9cd
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-12-17 20:16:12 -0800

    Fix crashes caused by the About dialog on older platform versions

--- app/src/main/java/com/ultramegatech/ey/AboutFragment.java
+++ app/src/main/java/com/ultramegatech/ey/AboutFragment.java
@@ -23,16 +23,16 @@
 package com.ultramegatech.ey;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -57,24 +57,12 @@ public class AboutFragment extends DialogFragment {
         fragment.show(fm, TAG);
     }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getContext())
-                .setTitle(R.string.titleAbout)
-                .setView(getLayout())
-                .create();
-    }
-
-    /**
-     * Get the layout for the Dialog.
-     *
-     * @return The View to place inside the Dialog
-     */
     @SuppressLint("InflateParams")
-    @NonNull
-    private View getLayout() {
-        final View root = LayoutInflater.from(getContext()).inflate(R.layout.fragment_about, null);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final View root = inflater.inflate(R.layout.fragment_about, null, false);
 
         ((TextView)root.findViewById(R.id.version)).setText(BuildConfig.VERSION_NAME);
         root.findViewById(R.id.support).setOnClickListener(new View.OnClickListener() {
@@ -100,6 +88,12 @@ public class AboutFragment extends DialogFragment {
                         Calendar.getInstance().get(Calendar.YEAR)));
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getDialog().setTitle(R.string.titleAbout);
     }
 
     /**
